b0cc6df1c98adc41217d64dab6b23b876688557e
Arup <email.arupbasak@gmail.com>
2022-07-14 13:51:21 +0530

    Some Design Improvement

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ import link_downloader
 
 app = Flask(__name__)
 
+vid_q_list = []
+aud_q_list = []
+
 
 @app.route('/download/<string:key>')
 def download(key):
     path = library.get_temp(key)
-    print(path)
     if path:
         return send_file(path, as_attachment=True)
     else:
@@ -22,6 +24,11 @@ def video_downloader():
     if request.method == "GET":
         youtube_link = request.args.get('link')
         return redirect(url_for('download', key=link_downloader.download(youtube_link)))
+
+
+@app.route('/youtube_video_downloader/res')
+def choose_res():
+    return render_template('view_res_option.html', list=list)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,9 +44,6 @@ def main():
 
 @app.route('/youtube_video_downloader/', methods=['GET', 'POST'])
 def downloader():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Do Something':
-            print("Hello World")
     return render_template('download_link_files.html')
 
 
@@ -51,4 +55,4 @@ def downloader():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
